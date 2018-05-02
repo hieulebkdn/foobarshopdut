e1773af6d20ed3ef4b1e6e597927f00c786d48e4
SongTranVan <songtranvan2511@gmail.com>
2018-05-02 19:22:43 +0700

    paginate for shops

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,6 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.paginate(page: params[:page], :per_page => 5).order('created_at desc')
-
   end
 
   # GET /products/1
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 # Returns the full title on a per-page basis.
 def full_title(page_title = '')
-	base_title = "FooBarShop"
+	base_title = "FooBar Computer"
 	if page_title.empty?
 		base_title
 	else
