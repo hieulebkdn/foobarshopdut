2534a2adf8e6103387df43cb90a6680e911b3738
hieulebkdn <102150274@sv.dut.edu.vn>
2018-05-04 08:59:52 +0700

    edit view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	def index
 		@brandLogos = Brand.all
 		@trendingProduct = Product.all.order(view: :desc).limit(5)
-		@newestProduct = Product.all.order(created_at: :desc)
+		@lastestProduct = Product.all.order(created_at: :desc)
 		@cart = current_cart
 	end
 
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,6 +5,7 @@ class ShopsController < ApplicationController
       @listBrand.push([b.name,b.id])
     end
     @options = ["A-Z","Price: Low to High", "Price: High to Low"]
+
     @products = Product.paginate(page: params[:page], :per_page => 8).order('created_at asc')
   end
 
@@ -30,17 +31,16 @@ class ShopsController < ApplicationController
   end
 
   def filter
-    # @products = Product.paginate(page: params[:page], :per_page => 8).order('created_at asc')
     @listBrand = [["All brand", 0]]
     Brand.all.each do |b|
       @listBrand.push([b.name,b.id])
     end
     @options = ["A-Z","Price: Low to High", "Price: High to Low"]
+    
     byBrand = params[:brand_option].to_s
     byRating = params[:rating_option].to_s
     byPrice = params[:price_option].to_s
     @products = Product.above_rating(byRating).belong_to_brand(byBrand).price_range(byPrice).paginate(page: params[:page], :per_page => 8)
-    flash[:success] = "Brandid #{byBrand}, Rating #{byRating}, Price #{byPrice}" 
     render 'index'
   end
 
